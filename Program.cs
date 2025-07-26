91c892be62d6dd425497ee905e0de853de3bf0b7
Andre-CBS <andre.cbs@live.com>
2025-07-26 13:53:22 -0300

    Desafio concluídogit add Program.csgit add Program.csgit add Program.csgit add Program.csgit add Program.cs!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456789", modelo: "Nokia G50", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n------------------------\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "987654321", modelo: "IPhone 13", imei: "222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
